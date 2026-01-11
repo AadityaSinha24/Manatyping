6879948195391b313af82b33b4c3b96b49e1ab11
Tejas Jain <tejasjain096@gmail.com>
2026-01-11 17:37:48 +0530

    first commit

--- src/app.ts
+++ src/app.ts
@@ -16,13 +16,12 @@ const app = express();
 app.use(compression());
 app.use(passport.initialize());
 app.use(helmet());
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: process.env.CLIENT_URL,
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ dotenv.config();
 import app from "./app";
 import { connectDB } from "./config/db";
 
-const PORT: number = Number(process.env.PORT) || 5000;
+const PORT: number = Number(process.env.PORT) || 3000;
 
 connectDB();
 
