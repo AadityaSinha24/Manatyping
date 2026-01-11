2cc95b28bb3ecb348fc9d673f1072938b9dd3fbe
AadityaSinha24 <adityasinha21024@gmail.com>
2026-01-11 20:13:15 +0530

    Basic setup

--- src/middlewares/PageNotFound.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import { ExpressError } from "../utils/ExpressError";
-export const PageNotFound = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  next(new ExpressError(404, "Page Not Found"));
-};
--- src/middlewares/isTeacher.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import { ExpressError } from "../utils/ExpressError";
-
-export const isTeacher = (req: Request, res: Response, next: NextFunction) => {
-  if ((req as any).user.role !== "teacher") {
-    return next(new ExpressError(403, "Access denied. Teacher only route."));
-  }
-  next();
-};
